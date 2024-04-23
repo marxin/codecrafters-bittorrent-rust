eee2458faf5bc66a09745ae93b1124dc46dfa5b8
Martin Liska <martin.liska@hey.com>
2024-04-23 17:20:17 +0200

    Implement string decoding

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,55 @@
-use serde_json;
-use std::env;
+use clap::{Parser, Subcommand};
+use serde_json::{Value};
 
-// Available if you need it!
-// use serde_bencode
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-#[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+#[derive(Subcommand)]
+enum Commands {
+    /// Decode a bencode object
+    Decode {
+        /// Encoded object value
+        object: String,
+    },
+}
+
+fn parse_bencode_value(value: &str) -> anyhow::Result<(Value, &str)> {
+    match value.chars().next() {
+        Some('0'..='9') => {
+            let (length, content) = value
+                .split_once(':')
+                .ok_or(anyhow::anyhow!("cannot find :"))?;
+            let length = length.parse::<usize>()?;
+            if content.len() < length {
+                anyhow::bail!(
+                    "encoded string is short: {}, expected: {length}",
+                    content.len()
+                );
+            }
+            Ok((
+                Value::String(content[..length].to_string()),
+                &content[length..],
+            ))
+        }
+        Some(_) => todo!(),
+        None => Ok((Value::Null, "")),
     }
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = &args[1];
-
-    if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
-    } else {
-        println!("unknown command: {}", args[1])
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Decode { object } => {
+            let value = parse_bencode_value(&object);
+            match value {
+                Ok(value) => println!("{}", value.0),
+                Err(err) => eprintln!("decode failed: {err}"),
+            }
+        }
     }
 }
